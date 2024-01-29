b091bb326ffe011de5eeafb60ced0edbf1f361cb
Tynbaev Maden <130825901+IDGAFM@users.noreply.github.com>
2024-01-30 01:01:10 +0600

    v2.4
    добавлен функционал входа и регистраций

--- Interface_of_Bank_System.py
+++ Interface_of_Bank_System.py
@@ -5,7 +5,7 @@ import random
 from config import *
 import requests
 
-# Функция генерации случайного ИИН
+
 def generate_random_inn():
     year = str(random.randint(0, 99)).zfill(2)
     month = str(random.randint(1, 12)).zfill(2)
@@ -13,13 +13,12 @@ def generate_random_inn():
     return year + month + day + ''.join([str(random.randint(0, 9)) for _ in range(6)])
 
 
-# Функция для отображения окна с данными о пользователях
 def show_users():
     user_window = tk.Toplevel(root)
     user_window.title("Данные о пользователях")
 
     try:
-        with psycopg2.connect(host=host, user=user, password=password, database=db_name) as connection:
+        with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
             print("Соединение прошло успешно.")
 
         with connection.cursor() as cur:
@@ -50,10 +49,9 @@ def show_users():
         print(f"Error in {er}")
 
 
-# Функция для отображения окна с данными о счетах
 def show_accounts():
     try:
-        with psycopg2.connect(host=host, user=user, password=password, database=db_name) as connection:
+        with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
             print("Соединение прошло успешно.")
 
         with connection.cursor() as cur:
@@ -68,7 +66,7 @@ def show_accounts():
 
 def show_payments_history():
     try:
-        with psycopg2.connect(host=host, user=user, password=password, database=db_name) as connection:
+        with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
             print("Соединение прошло успешно.")
 
         with connection.cursor() as cur:
@@ -83,7 +81,7 @@ def show_payments_history():
 
 def show_payments():
     try:
-        with psycopg2.connect(host=host, user=user, password=password, database=db_name) as connection:
+        with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
             print("Соединение прошло успешно.")
 
         with connection.cursor() as cur:
@@ -96,36 +94,28 @@ def show_payments():
         print(f"Error in {er}")
 
 
-# Функция для заявки на кредит
 def apply_for_credit():
     messagebox.showinfo("Кредит", "Вы успешно подали заявку на кредит!")
 
 
-# Функция для отображения текущих кредитов у пользователя
 def show_user_credits():
-    # Реализация отображения текущих кредитов у пользователя
     pass
 
 
-# Функция для отображения курсов валют
-
 def show_currency_rates():
-
     try:
         api_url = "https://api.exchangerate-api.com/v4/latest/KZT"
         response = requests.get(api_url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response.raise_for_status()
         data = response.json()
 
         usd_to_kzt_rate = round(1 / data["rates"]["USD"], 1)
         eur_to_kzt_rate = round(1 / data["rates"]["EUR"], 2)
         rub_to_kzt_rate = round(1 / data["rates"]["RUB"], 2)
 
-        # Создание нового окна для отображения курсов валют
         currency_rates_window = Toplevel()
         currency_rates_window.title("Курсы валют")
 
-        # Отображение полученных данных
         tk.Label(currency_rates_window, text=f"USD/KZT: {usd_to_kzt_rate}").pack()
         tk.Label(currency_rates_window, text=f"EUR/KZT: {eur_to_kzt_rate}").pack()
         tk.Label(currency_rates_window, text=f"RUB/KZT: {rub_to_kzt_rate}").pack()
@@ -134,24 +124,36 @@ def show_currency_rates():
         messagebox.showerror("Ошибка", f"Не удалось получить курсы валют: {e}")
 
 
-# Функция для заявки на страховой полис
 def apply_for_insurance_policy():
     messagebox.showinfo("Страховой полис", "Вы успешно подали заявку на страховой полис!")
 
 
-# Функция для отображения текущих страховых полисов
 def show_insurance_policies():
     messagebox.showinfo("Страховые полисы", "Отображение текущих страховых полисов")
 
 
-# Функция для отображения списка филиалов
 def show_branches():
-    # Реализация отображения списка филиалов
     pass
 
 
-# Функция для отображения окна входа
 def open_login_window():
+    def login(username, password):
+        try:
+            with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
+                with connection.cursor() as cur:
+                    cur.execute("SELECT * FROM users WHERE iin = %s AND password = %s", (username, password))
+                    user_data = cur.fetchone()
+
+                    if user_data:
+                        messagebox.showinfo("Успех", "Вы успешно вошли в систему!")
+                        login_window.destroy()
+                        show_main_window()
+                    else:
+                        messagebox.showerror("Ошибка", "Неверные данные пользователя!")
+
+        except psycopg2.Error as er:
+            messagebox.showerror("Ошибка", f"Ошибка при входе: {er}")
+
     login_window = tk.Toplevel(root)
     login_window.title("Вход")
 
@@ -169,13 +171,20 @@ def open_login_window():
                           font=("Arial", 14))
     btn_login.pack(pady=20)
 
-    def login(username, password):
-        messagebox.showinfo("Успех", "Вы успешно вошли в систему!")
-        login_window.destroy()  # Закрытие окна входа,
-        show_main_window()
-
 
 def open_register_window():
+    def register(username, password):
+        try:
+            with psycopg2.connect(host=host, user=user, password=password, database=db_name, client_encoding='utf8') as connection:
+                with connection.cursor() as cur:
+                    cur.execute("INSERT INTO users (iin, password) VALUES (%s, %s)", (username, password))
+                    messagebox.showinfo("Успех", "Регистрация прошла успешно!")
+                    register_window.destroy()
+                    show_main_window()
+
+        except psycopg2.Error as er:
+            messagebox.showerror("Ошибка", f"Ошибка при регистрации: {er}")
+
     register_window = tk.Toplevel(root)
     register_window.title("Регистрация")
 
@@ -193,31 +202,22 @@ def open_register_window():
                              command=lambda: register(entry_username.get(), entry_password.get()), font=("Arial", 14))
     btn_register.pack(pady=20)
 
-    def register(username, password):
-        messagebox.showinfo("Успех", "Регистрация прошла успешно!")
-        register_window.destroy()
-        show_main_window()
-
 
 def show_main_window():
-    root.withdraw()  # Скрываем окно входа/регистрации
-    main_window = tk.Tk()  # Создаем новое главное окно
+    root.withdraw()
+    main_window = tk.Tk()
     main_window.title("Банковская система")
     main_window.geometry('750x750')
 
-    # Создаем меню в главном окне
     menubar = tk.Menu(main_window)
     main_window.config(menu=menubar)
 
-    # Добавляем пункты меню
     options_menu = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label="Кредиты", menu=options_menu)
 
-    # Добавляем команды в подменю
     options_menu.add_command(label="Текущие кредиты", command=show_currency_rates)
     options_menu.add_command(label="Заявка на кредит", command=apply_for_credit)
 
-    # Добавляем пункты меню для страховых полисов
     insurance_menu = tk.Menu(menubar, tearoff=0)
     menubar.add_cascade(label="Страховые полисы", menu=insurance_menu)
     insurance_menu.add_command(label="Просмотр текущих полисов", command=show_insurance_policies)
@@ -228,8 +228,6 @@ def show_main_window():
     payment_menu.add_command(label="История платежей", command=show_payments_history)
     payment_menu.add_command(label="Платежи", command=show_payments_history)
 
-
-    # Создаем кнопки в главном окне
     users_button = tk.Button(main_window, text="Пользователи", command=show_users, font=("Arial", 14))
     accounts_button = tk.Button(main_window, text="Счета", command=show_accounts, font=("Arial", 14))
     currency_rates_button = tk.Button(main_window, text="Курсы валют", command=show_currency_rates, font=("Arial", 14))
@@ -248,12 +246,10 @@ root = tk.Tk()
 root.title("Вход/Регистрация")
 root.geometry('400x300')
 
-# Sign in
-user_login = tk.Button(root, text="Sign in", command=open_login_window, font=("Arial", 14))
+user_login = tk.Button(root, text="Войти", command=open_login_window, font=("Arial", 14))
 user_login.pack(pady=20)
 
-# Sign up
-user_register = tk.Button(root, text="Sign up", command=open_register_window, font=("Arial", 14))
+user_register = tk.Button(root, text="Зарегистрироваться", command=open_register_window, font=("Arial", 14))
 user_register.pack(pady=20)
 
 root.mainloop()
